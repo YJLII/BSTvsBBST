545224534dc818e0c2273949329d70bdb95cf99f
YJLII <destprato@gmail.com>
2021-05-18 00:55:10 +0900

    뼈대 다 맞춤

--- Project8/hw6_2021.c
+++ Project8/hw6_2021.c
@@ -1,4 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+#include<time.h>
+#include<malloc.h>
+#ifndef max
+#define max(a,b)  (((a) > (b)) ? (a) : (b))
+#endif
+
 struct BinarySearchTreeNode {
     int data;
     struct BinarySearchTreeNode* left;
@@ -12,18 +18,18 @@ struct AVLTreeNode {
     int height;
 }AVL;
 
-struct BinarySearchTreeNode* Find(struct BinarySearchTreeNode* root, int data) {
+struct BinarySearchTreeNode* BSTFind(struct BinarySearchTreeNode* root, int data) {
     if (root == NULL)
         return NULL;
     if (data < root->data)
-        return Find(root->left, data);
+        return BSTFind(root->left, data);
     else if (data > root->data)
-        return Find(root->right, data);
+        return BSTFind(root->right, data);
     return root;
 }
 
 
-struct BinarySearchTreeNode* Insert(struct BinarySearchTreeNode* root, int data) {
+struct BinarySearchTreeNode* BSTInsert(struct BinarySearchTreeNode* root, int data) {
     if (root == NULL) {
         root = (struct BinarySearchTreeNode*)malloc(sizeof(struct BinarySearchTreeNode));
         if (root == NULL) {
@@ -36,12 +42,42 @@ struct BinarySearchTreeNode* Insert(struct BinarySearchTreeNode* root, int data)
         }
     }
     else {
-        if (data < root->data)   root->left = Insert(root->left, data);
-        else if (data > root->data)   root->right = Insert(root->right, data);
+        if (data < root->data)   root->left = BSTInsert(root->left, data);
+        else if (data > root->data)   root->right = BSTInsert(root->right, data);
     }
     return root;
 }//For Number 1
-struct AVLTreeNode* Insert(struct AVLTreeNode* root, int data) {
+int Height(struct AVLTreeNode* root) {
+    if (!root) return -1;
+    else return root->height;
+}
+
+struct AVLTreeNode* SingleRotateLeft(struct AVLTreeNode* X) {
+    struct AVLTreeNode* W = X->left;
+    X->left = W->right;
+    W->right = X;
+    X->height = max(Height(X->left), Height(X->right)) + 1;
+    W->height = max(Height(W->left), X->height) + 1;
+    return W; //새 루트 노드
+}
+struct AVLTreeNode* SingleRotateRight(struct AVLTreeNode* W) {
+    struct AVLTreeNode* X = W->right;
+    W->right = X->left;
+    X->left = W;
+    W->height = max(Height(W->left), Height(W->right)) + 1;
+    X->height = max(W->height, Height(X->right)) + 1;
+    return X;
+}
+struct AVLTreeNode* DoubleRotatewithLeft(struct AVLTreeNode* Z) {
+    Z->left = SingleRotateRight(Z->left);
+    return  SingleRotateLeft(Z);
+}
+struct AVLTreeNode* DoubleRotatewithRight(struct AVLTreeNode* X) {
+    X->right = SingleRotateLeft(X->right);
+    return  SingleRotateRight(X);
+}
+
+struct AVLTreeNode* AVLInsert(struct AVLTreeNode* root, int data) {
     if (!root) {
         root = (struct AVLTreeNode*)malloc(sizeof(struct AVLTreeNode*));
         if (!root) {
@@ -55,7 +91,7 @@ struct AVLTreeNode* Insert(struct AVLTreeNode* root, int data) {
         }
     }
     else if (data < root->data) {  // 왼쪽으로
-        root->left = Insert(root->left, data);
+        root->left = AVLInsert(root->left, data);
         if ((Height(root->left) - Height(root->right)) == 2) {
             if (data < root->left->data)    // LeftLeft
                 root = SingleRotateLeft(root);
@@ -63,7 +99,7 @@ struct AVLTreeNode* Insert(struct AVLTreeNode* root, int data) {
         }
     }
     else if (data > root->data) {  // 오른쪽으로
-        root->right = Insert(root->right, data);
+        root->right = AVLInsert(root->right, data);
         if ((Height(root->right) - Height(root->left)) == 2) {
             if (data > root->right->data)    // RightRight
                 root = SingleRotateRight(root);
@@ -75,7 +111,15 @@ struct AVLTreeNode* Insert(struct AVLTreeNode* root, int data) {
     return root;
 }
 
+
+void forQuestion01(){
+    srand(time(NULL));
+    int RandomNumber = (rand() * 3)%100001;
+    printf("%d", RandomNumber);
+
+}
+
 int main(void)
 {
-
+    forQuestion01();
 }
